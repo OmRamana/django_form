cc8cb08873b63fcdd520e54ecd47863ddf83597e
Om <ramanacomfy@gmail.com>
2022-01-21 13:50:21 -0800

    bootstrap django form

--- simple_form/forms.py
+++ simple_form/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    search_query = forms.CharField(label = "Search",max_length=100)
+    search_query = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
