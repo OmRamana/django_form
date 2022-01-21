8c1b00ed1d8b9dd12501caa0ab673fba42118031
Om <ramanacomfy@gmail.com>
2022-01-21 13:54:23 -0800

    remove form label

--- simple_form/forms.py
+++ simple_form/forms.py
@@ -1,5 +1,6 @@
+from re import L
 from django import forms
 
 class SearchForm(forms.Form):
-    search_query = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    search_query = forms.CharField(label='',max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
